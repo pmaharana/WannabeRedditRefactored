d628091ab60a5de1b5ef3ce454de24a1aabff919
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-21 20:04:42 -0400

    --dynamic and crud made

--- WannabeReddit/Models/Post.cs
+++ WannabeReddit/Models/Post.cs
@@ -12,6 +12,7 @@ namespace WannabeReddit.Models
         public string Body { get; set; }
         public int? UpVotes { get; set; }
         public int? DownVotes { get; set; }
+        public DateTime? DatePosted { get; set; } = DateTime.Now;
 
         public int? UserId { get; set; }
         public ApplicationUser User { get; set; }
